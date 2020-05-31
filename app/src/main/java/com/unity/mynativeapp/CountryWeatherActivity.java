c5b83d86765c1d7bb06fc8c0f714028ab35773b7
JoAMD <joasus12345@gmail.com>
2020-06-01 02:19:28 +0530

    Got HttpsURLConnection + AsyncTask<> working at last

--- app/src/main/java/com/unity/mynativeapp/CountryWeatherActivity.java
+++ app/src/main/java/com/unity/mynativeapp/CountryWeatherActivity.java
@@ -2,6 +2,7 @@ package com.unity.mynativeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class CountryWeatherActivity extends AppCompatActivity {
@@ -12,6 +13,8 @@ public class CountryWeatherActivity extends AppCompatActivity {
         setContentView(R.layout.activity_country_weather);
 
         TextView textView = findViewById(R.id.countryName);
-        textView.setText(getIntent().getStringExtra("countryName"));
+        String res = getIntent().getStringExtra("countryName");
+        Log.d("nm", "country name in new activity = " + res);
+        textView.setText(res);
     }
 }
--- app/src/main/java/com/unity/mynativeapp/TestLearnActivity.java
+++ app/src/main/java/com/unity/mynativeapp/TestLearnActivity.java
@@ -1,8 +1,10 @@
 package com.unity.mynativeapp;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,14 +12,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
-import java.security.cert.Certificate;
 
 import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLPeerUnverifiedException;
 
 public class TestLearnActivity extends AppCompatActivity {
 
@@ -38,68 +38,179 @@ public class TestLearnActivity extends AppCompatActivity {
         URL url;
         try{
             url = new URL(getResources().getString(R.string.rest_api_countries_name));
-            HttpURLConnection conn = new HttpsURLConnection(url) {
-                @Override
-                public String getCipherSuite() {
-                    return null;
+
+//            String resultStr = new RetrieveJSONData().execute().get();
+
+            /////////////RetrieveJSONData data = new RetrieveJSONData();
+            /////////////          data.SetActivity(this);
+            /////////// String resultStr = data.doInBackground(url);
+//            String resultStr = new RetrieveJSONData().execute(url);
+
+            new RetrieveJSONData().execute(url);
+
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void CallNextActivity(String countryName){
+        Intent intent = new Intent(getApplicationContext(), CountryWeatherActivity.class);
+        intent.putExtra("countryName", countryName);
+        startActivity(intent);
+    }
+
+    private class RetrieveJSONData extends AsyncTask<URL, Void, String> {
+
+        HttpsURLConnection conn;
+        //    ProgressBar progressBar;
+//    ProgressDialog dialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+//        progressBar.show
+//        dialog = ProgressDialog.show()
+            Log.d("nm", "Started fetching!");
+        }
+
+        @Override
+        protected String doInBackground(URL... urls) {
+
+            try{
+
+                Log.d("nm", "background task");
+                int count = urls.length;
+                for (int i = 0; i < count; i++) {
+                    conn = (HttpsURLConnection) urls[i].openConnection();
+
+                    conn.setConnectTimeout(5000);
+                    conn.setRequestMethod("GET");
+
+                    Object obj = conn.getContent();
+                    InputStream inputStream = (InputStream) obj;
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                    String line;
+                    StringBuilder result = new StringBuilder();
+                    while ((line = bufferedReader.readLine()) != null){
+                        result.append(line);
+                    }
+
+
+                    return result.toString();
                 }
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+            return null;
+        }
 
-                @Override
-                public Certificate[] getLocalCertificates() {
-                    return new Certificate[0];
-                }
+        @Override
+        protected void onPostExecute(String str) {
+            super.onPostExecute(str);
+            Log.d("nm", "Done fetching!");
 
-                @Override
-                public Certificate[] getServerCertificates() throws SSLPeerUnverifiedException {
-                    return new Certificate[0];
-                }
+            JSONObject countries = null;
+            String countryName = null;
+            try {
+                countries = new JSONArray(str).getJSONObject(0);
+                countryName = countries.getString("name");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
 
-                @Override
-                public void disconnect() {
-
-                }
-
-                @Override
-                public boolean usingProxy() {
-                    return false;
-                }
-
-                @Override
-                public void connect() throws IOException {
-
-                }
-            };
-
-            conn.setDoOutput(true);
-
-            String response = conn.getResponseMessage();
-
-            JSONArray countries = new JSONArray(response).getJSONArray(0);
-
-            String countryName = countries.getJSONObject(0).getString("name");
 
             TextView textView = findViewById(R.id.city_name);
             textView.setText(countryName);
 
             CallNextActivity(countryName);
-
         }
-        catch (MalformedURLException e){
-            textView2.setText(textView2.getText() + e.getMessage());
-            e.printStackTrace();
-        } catch (IOException e) {
-            textView2.setText(textView2.getText() + e.getMessage());
-            e.printStackTrace();
-        } catch (JSONException e) {
-            textView2.setText(textView2.getText() + e.getMessage());
-            e.printStackTrace();
-        }
-    }
 
-    private void CallNextActivity(String countryName){
-        Intent intent = new Intent(getApplicationContext(), CountryWeatherActivity.class);
-        intent.putExtra("country name", countryName);
-        startActivity(intent);
+//        @Override
+//        protected void onPostExecute() {
+//            super.onPostExecute();
+//            Log.d("nm", "Done fetching!");
+//
+////            CallNextActivity(countryName);
+//        }
     }
 
 }
+
+//class RetrieveJSONData extends AsyncTask<URL, Void, String> {
+//
+//    HttpsURLConnection conn;
+////    ProgressBar progressBar;
+////    ProgressDialog dialog;
+//    TestLearnActivity activity;
+//
+//    @Override
+//    protected void onPreExecute() {
+//        super.onPreExecute();
+////        progressBar.show
+////        dialog = ProgressDialog.show()
+//        Log.d("nm", "Started fetching!");
+//    }
+//
+//    @Override
+//    protected String doInBackground(URL... urls) {
+//
+//        try{
+//
+//            int count = urls.length;
+//            for (int i = 0; i < count; i++) {
+//                conn = (HttpsURLConnection) urls[i].openConnection();
+//
+//                conn.setConnectTimeout(5);
+//                conn.setRequestMethod("GET");
+//
+//                Object obj = conn.getContent();
+//                InputStream inputStream = (InputStream) obj;
+//                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+//                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+//                String line;
+//                StringBuilder result = new StringBuilder();
+//                while ((line = bufferedReader.readLine()) != null){
+//                    result.append(line);
+//                }
+//
+//
+//                return result.toString();
+//            }
+//
+//        }
+//        catch(Exception e){
+//            e.printStackTrace();
+//        }
+//
+//        return null;
+//    }
+//
+//    public void SetActivity(TestLearnActivity activity){
+//        this.activity = activity;
+//    }
+//
+//    @Override
+//    protected void onPostExecute(String resultStr) {
+//        super.onPostExecute(resultStr);
+//        conn.disconnect();
+//
+//        JSONArray countries = null;
+//        String countryName = null;
+//        try {
+//            countries = new JSONArray(resultStr).getJSONArray(0);
+//            countryName = countries.getJSONObject(0).getString("name");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        TextView textView = activity.findViewById(R.id.city_name);
+//        textView.setText(countryName);
+//        Log.d("nm", "Done fetching!");
+//
+//        activity.CallNextActivity(countryName);
+//    }
+//}
